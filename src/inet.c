e22e45b8f6342f6454ad280fcdcde50f8e50af2d
Nick Briggs <nicholas.h.briggs@gmail.com>
2017-07-04 15:12:43 -0700

    include unistd.h for getpid/read/close in inet.c

--- src/inet.c
+++ src/inet.c
@@ -37,6 +37,7 @@ static char *id = "$Id: inet.c,v 1.3 2001/12/24 01:09:03 sybalsky Exp $ Copyrigh
 #include <netdb.h>
 #include <sys/ioctl.h>
 #include <errno.h>
+#include <unistd.h>
 #endif /* DOS */
 #include "lispemul.h"
 #include "lispmap.h"
