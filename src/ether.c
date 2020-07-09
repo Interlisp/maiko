62b8f464059c868434884700063b97bdd22a09bd
Nick Briggs <nicholas.h.briggs@gmail.com>
2020-07-09 10:16:41 -0700

    Add forward declaration for get_packet()
    	modified:   src/ether.c

--- src/ether.c
+++ src/ether.c
@@ -365,6 +365,7 @@ LispPTR ether_reset(LispPTR args[])
 /*	it's OK to read packets from the network on interrupt.		*/
 /*									*/
 /************************************************************************/
+LispPTR get_packet();
 
 LispPTR ether_get(LispPTR args[])
 {
