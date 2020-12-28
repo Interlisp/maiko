023cf609f2ecea6be9c38ab5f14abbe671d8ecd6
Bruce Mitchener <bruce.mitchener@gmail.com>
2020-12-28 14:19:30 +0700

    init_Xcursor takes Window, not int for window. (#137)
    `Window` is an X11 typedef for `unsigned long` (via `XID`). We should
    pass it as that, not an `int`, to avoid 64 bit conversion issues.

--- inc/xcursordefs.h
+++ inc/xcursordefs.h
@@ -4,6 +4,6 @@
 #include "devif.h"
 void Init_XCursor(void);
 void Set_XCursor(int x, int y);
-void init_Xcursor(Display *display, int window);
+void init_Xcursor(Display *display, Window window);
 void set_Xcursor(DspInterface dsp, unsigned char *bitmap, int hotspot_x, int hotspot_y, Cursor *return_cursor, int from_lisp);
 #endif
