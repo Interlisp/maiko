5fb1039805d220a9c21d7b6f217f7b976aeff2f9
Nick Briggs <nicholas.h.briggs@gmail.com>
2021-01-06 00:43:02 -0500

    Clean up warnings when compiling with ethernet enabled
    Add a dlpidefs.h header for prototypes.
    Convert from bcopy() to memcpy().
    Change some char to u_char as needed.

---
+++ inc/dlpidefs.h
@@ -0,0 +1,8 @@
+#ifndef DLPIDEFS_H
+#define DLPIDEFS_H 1
+int setup_dlpi_dev(char *device);
+void flush_dlpi(int fd);
+int dlpi_devtype(int fd);
+char *savestr(char *s);
+int dlunitdatareq(int fd, u_char *addrp, int addrlen, u_long minpri, u_long maxpri, u_char *datap, int datalen);
+#endif
