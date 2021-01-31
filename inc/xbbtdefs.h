c0ccb158ea8929bad68d84e9f7e5d60e6a9688e7
Nick Briggs <nicholas.h.briggs@gmail.com>
2021-01-30 16:53:18 -0800

    Fix off-by-one error in clipping_Xbitblt
    The LowerRightX and LowerRightY were calculated as the X and Y coordinates of the
    pixel to the right and below the last visible pixel, but are compared with the
    last actually visible pixel in the source. They need an extra -1 offset to correct.
    
    Add comments to explain what's going on.  Fix the signature of the function to reflect
    that the "dummy" passed for the display region is a pointer not an integer.

--- inc/xbbtdefs.h
+++ inc/xbbtdefs.h
@@ -1,4 +1,4 @@
 #ifndef XBBTDEFS_H
 #define XBBTDEFS_H 1
-unsigned long clipping_Xbitblt(DspInterface dsp, int dummy, int x, int y, int w, int h);
+unsigned long clipping_Xbitblt(DspInterface dsp, DLword *dummy, int x, int y, int w, int h);
 #endif
