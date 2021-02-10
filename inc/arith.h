e511b6a7c487b1b0f8eb6048b0a18a97004e9b49
Nick Briggs <nicholas.h.briggs@gmail.com>
2021-02-10 15:10:44 -0800

    Reformat (with clang-format 10.0) some header files
    ...in preparation for editing macro definitions to wrap in do {} while (0)
    or other adjustment to make them complete statements without unnecessary
    semicolons.
    
    This programmatic reformatting should not introduce any functional changes.

--- inc/arith.h
+++ inc/arith.h
@@ -1,10 +1,6 @@
 #ifndef ARITH_H
 #define ARITH_H 1
-/* $Id: arith.h,v 1.2 1999/01/03 02:05:52 sybalsky Exp $ (C) Copyright Venue, All Rights Reserved  */
-
-
-
-
+/* $Id: arith.h,v 1.2 1999/01/03 02:05:52 sybalsky Exp $ (C) Copyright Venue, All Rights Reserved */
 
 /************************************************************************/
 /*									*/
@@ -13,168 +9,151 @@
 /*									*/
 /************************************************************************/
 
+#define MAX_SMALL 65535  /* == 0x0000FFFF  */
+#define MIN_SMALL -65536 /* == 0xFFFF0000  */
 
-#define	MAX_SMALL	65535		/* == 0x0000FFFF  */
-#define	MIN_SMALL	-65536		/* == 0xFFFF0000  */
+#define MAX_FIXP 2147483647  /* == 0x7FFFFFFF  */
+#define MIN_FIXP -2147483648 /* == 0x80000000  */
 
-#define MAX_FIXP	2147483647	/* == 0x7FFFFFFF  */
-#define	MIN_FIXP	-2147483648	/* == 0x80000000  */
+#define GetSmalldata(x)          \
+  (((SEGMASK & x) == S_POSITIVE) \
+       ? (0xFFFF & x)            \
+       : (((SEGMASK & x) == S_NEGATIVE) ? (0xFFFF0000 | x) : error("Not smallp address")))
 
-#define GetSmalldata(x)	(((SEGMASK & x)==S_POSITIVE) ? (0xFFFF & x) : (((SEGMASK & x)==S_NEGATIVE) ? (0xFFFF0000 | x) : error("Not smallp address")))
-
-#define GetSmallp(x)	((0xFFFF0000 & x) ? (((0xFFFF0000 & x)==0xFFFF0000) ? (S_NEGATIVE | (0xFFFF & x)) : error("Not Smallp data") ) : (S_POSITIVE | (0xFFFF & x)))
-
+#define GetSmallp(x)                                                                  \
+  ((0xFFFF0000 & x) ? (((0xFFFF0000 & x) == 0xFFFF0000) ? (S_NEGATIVE | (0xFFFF & x)) \
+                                                        : error("Not Smallp data"))   \
+                    : (S_POSITIVE | (0xFFFF & x)))
 
 #define FIXP_VALUE(dest) *((int *)Addr68k_from_LADDR(dest))
 
 #define FLOATP_VALUE(dest) *((float *)Addr68k_from_LADDR(dest))
 
-#define N_GETNUMBER(sour, dest, label)					\
-{		dest = sour;	/* access memory once */ 		\
-		switch(SEGMASK & dest){				\
-		case S_POSITIVE:					\
-			dest = 0xFFFF & (dest);				\
-			break;						\
-		case S_NEGATIVE:					\
-			dest = 0xFFFF0000 | (dest);			\
-			break;						\
-		default:						\
-			if (GetTypeNumber( dest ) != TYPE_FIXP)		\
-				{ goto label; }				\
-			dest = FIXP_VALUE(dest);			\
-		}							\
-}
+#define N_GETNUMBER(sour, dest, label)                        \
+  {                                                           \
+    dest = sour; /* access memory once */                     \
+    switch (SEGMASK & dest) {                                 \
+      case S_POSITIVE: dest = 0xFFFF & (dest); break;         \
+      case S_NEGATIVE: dest = 0xFFFF0000 | (dest); break;     \
+      default:                                                \
+        if (GetTypeNumber(dest) != TYPE_FIXP) { goto label; } \
+        dest = FIXP_VALUE(dest);                              \
+    }                                                         \
+  }
 
-#define N_IGETNUMBER(sour, dest, label)					\
-{		dest = sour;	/* access memory once */ 		\
-		switch(SEGMASK & dest){				\
-		case S_POSITIVE:					\
-			dest = 0xFFFF & dest;				\
-			break;						\
-		case S_NEGATIVE:					\
-			dest = 0xFFFF0000 | dest;			\
-			break;						\
-		default:						\
-			switch (GetTypeNumber( dest )) {		\
-			case TYPE_FIXP:					\
-			  dest = FIXP_VALUE(dest);			\
-			  break;					\
-			case TYPE_FLOATP:				\
-			  {register float temp;				\
-			   temp = FLOATP_VALUE(dest) ;			\
-			   if (	(temp > ((float) 0x7fffffff)) ||	\
-				(temp < ((float) 0x80000000)) )		\
-				goto label;				\
-			   dest = (int) temp;				\
-			   }						\
-			  break;					\
-			default: goto label;				\
-			}						\
-			break;						\
-		}							\
-}
+#define N_IGETNUMBER(sour, dest, label)                                                   \
+  {                                                                                       \
+    dest = sour; /* access memory once */                                                 \
+    switch (SEGMASK & dest) {                                                             \
+      case S_POSITIVE: dest = 0xFFFF & dest; break;                                       \
+      case S_NEGATIVE: dest = 0xFFFF0000 | dest; break;                                   \
+      default:                                                                            \
+        switch (GetTypeNumber(dest)) {                                                    \
+          case TYPE_FIXP: dest = FIXP_VALUE(dest); break;                                 \
+          case TYPE_FLOATP: {                                                             \
+            register float temp;                                                          \
+            temp = FLOATP_VALUE(dest);                                                    \
+            if ((temp > ((float)0x7fffffff)) || (temp < ((float)0x80000000))) goto label; \
+            dest = (int)temp;                                                             \
+          } break;                                                                        \
+          default: goto label;                                                            \
+        }                                                                                 \
+        break;                                                                            \
+    }                                                                                     \
+  }
 
-
-#define ARITH_SWITCH(arg, result)					\
-	switch((int) arg & 0xFFFF0000){					\
-		case 0:							\
-			result = (S_POSITIVE | (int) arg);		\
-			break;						\
-		case 0xFFFF0000:					\
-			result = (S_NEGATIVE | (0xFFFF & (int) arg));	\
-			break;						\
-		default:{register LispPTR *wordp;			\
-			/* arg is FIXP, call createcell */		\
-			wordp = (LispPTR *) createcell68k(TYPE_FIXP);	\
-			*((int *)wordp) = (int) arg;			\
-			result = (LADDR_from_68k(wordp));		\
-			break;						\
-			}						\
-	}
-
+#define ARITH_SWITCH(arg, result)                                        \
+  switch ((int)arg & 0xFFFF0000) {                                       \
+    case 0: result = (S_POSITIVE | (int)arg); break;                     \
+    case 0xFFFF0000: result = (S_NEGATIVE | (0xFFFF & (int)arg)); break; \
+    default: {                                                           \
+      register LispPTR *wordp;                                           \
+      /* arg is FIXP, call createcell */                                 \
+      wordp = (LispPTR *)createcell68k(TYPE_FIXP);                       \
+      *((int *)wordp) = (int)arg;                                        \
+      result = (LADDR_from_68k(wordp));                                  \
+      break;                                                             \
+    }                                                                    \
+  }
 
 /* *******
-	NEED to See if this is faster than the N_ARITH_SWITCH macro
+        NEED to See if this is faster than the N_ARITH_SWITCH macro
 
-	if( (MIN_FIXP <= result) && (result <= MAX_FIXP) ){
-		if(0 <= result){
-			if(result <= MAX_SMALL)
-				return(S_POSITIVE | result);
-			else{
-				wordp = createcell68k(TYPE_FIXP);
-				*((unsigned int *)wordp) = result;
-				return(LADDR_from_68k(wordp));
-			}
-		}else{
-			if(MIN_SMALL <= result)
-				return(S_NEGATIVE | (0xFFFF & result));
-			else{
-				wordp = createcell68k(TYPE_FIXP);
-				*((unsigned int *)wordp) = result;
-				return(LADDR_from_68k(wordp));
-			}
-		}/ 
-	}
+        if( (MIN_FIXP <= result) && (result <= MAX_FIXP) ){
+                if(0 <= result){
+                        if(result <= MAX_SMALL)
+                                return(S_POSITIVE | result);
+                        else{
+                                wordp = createcell68k(TYPE_FIXP);
+                                *((unsigned int *)wordp) = result;
+                                return(LADDR_from_68k(wordp));
+                        }
+                }else{
+                        if(MIN_SMALL <= result)
+                                return(S_NEGATIVE | (0xFFFF & result));
+                        else{
+                                wordp = createcell68k(TYPE_FIXP);
+                                *((unsigned int *)wordp) = result;
+                                return(LADDR_from_68k(wordp));
+                        }
+                }/
+        }
 ****** */
 
+#define N_ARITH_SWITCH(arg)                                \
+  switch (arg & 0xFFFF0000) {                              \
+    case 0: return (S_POSITIVE | arg);                     \
+    case 0xFFFF0000: return (S_NEGATIVE | (0xFFFF & arg)); \
+    default: {                                             \
+      register LispPTR *fixpp;                             \
+      /* arg is FIXP, call createcell */                   \
+      fixpp = (LispPTR *)createcell68k(TYPE_FIXP);         \
+      *((int *)fixpp) = arg;                               \
+      return (LADDR_from_68k(fixpp));                      \
+    }                                                      \
+  }
 
+#define N_IARITH_BODY_2(a, tos, op)  \
+  {                                  \
+    register int arg1, arg2;         \
+                                     \
+    N_IGETNUMBER(a, arg1, do_ufn);   \
+    N_IGETNUMBER(tos, arg2, do_ufn); \
+                                     \
+    arg1 = arg1 op arg2;             \
+                                     \
+    N_ARITH_SWITCH(arg1);            \
+                                     \
+  do_ufn:                            \
+    ERROR_EXIT(tos);                 \
+  }
 
-#define N_ARITH_SWITCH(arg)						\
-	switch(arg & 0xFFFF0000){					\
-		case 0:							\
-			return(S_POSITIVE | arg);			\
-		case 0xFFFF0000:					\
-			return(S_NEGATIVE | (0xFFFF & arg));		\
-		default:{register LispPTR *fixpp;			\
-			/* arg is FIXP, call createcell */		\
-			fixpp = (LispPTR *) createcell68k(TYPE_FIXP);	\
-			*((int *)fixpp) = arg;				\
-			return(LADDR_from_68k(fixpp));			\
-			}						\
-	}
+#define N_ARITH_BODY_1(a, n, op)  \
+  {                               \
+    register int arg1;            \
+                                  \
+    N_GETNUMBER(a, arg1, do_ufn); \
+                                  \
+    arg1 = arg1 op n;             \
+                                  \
+    N_ARITH_SWITCH(arg1);         \
+                                  \
+  do_ufn:                         \
+    ERROR_EXIT(a);                \
+  }
 
-
-#define N_IARITH_BODY_2(a, tos, op)					\
-{									\
-register int	arg1,arg2;						\
-									\
-	N_IGETNUMBER( a, arg1, do_ufn);					\
-	N_IGETNUMBER( tos, arg2, do_ufn);				\
-									\
-	arg1 = arg1 op arg2;						\
-									\
-	N_ARITH_SWITCH(arg1);						\
-									\
-do_ufn:	ERROR_EXIT(tos);						\
-}
-
-
-#define N_ARITH_BODY_1(a, n, op)					\
-{									\
-register int	arg1;							\
-									\
-	N_GETNUMBER( a, arg1, do_ufn);					\
-									\
-	arg1 = arg1 op n;						\
-									\
-N_ARITH_SWITCH(arg1);							\
-									\
-do_ufn:	ERROR_EXIT(a);							\
-}
-
-
-#define N_ARITH_BODY_1_UNSIGNED(a, n, op)				\
-{									\
-register unsigned int	arg1;						\
-									\
-	N_GETNUMBER( a, arg1, do_ufn);					\
-									\
-	arg1 = arg1 op n;						\
-									\
-N_ARITH_SWITCH(arg1);							\
-									\
-do_ufn:	ERROR_EXIT(a);							\
-}
-
+#define N_ARITH_BODY_1_UNSIGNED(a, n, op) \
+  {                                       \
+    register unsigned int arg1;           \
+                                          \
+    N_GETNUMBER(a, arg1, do_ufn);         \
+                                          \
+    arg1 = arg1 op n;                     \
+                                          \
+    N_ARITH_SWITCH(arg1);                 \
+                                          \
+  do_ufn:                                 \
+    ERROR_EXIT(a);                        \
+  }
 
 #endif /* ARITH_H */
--- inc/tos1defs.h
+++ inc/tos1defs.h
@@ -1,8 +1,7 @@
 #ifndef TOS1DEFS_H
 #define TOS1DEFS_H 1
-/* $Id: tos1defs.h,v 1.2 1999/01/03 02:06:27 sybalsky Exp $ (C) Copyright Venue, All Rights Reserved  */
-
-
+/* $Id: tos1defs.h,v 1.2 1999/01/03 02:06:27 sybalsky Exp $ (C) Copyright Venue, All Rights Reserved
+ */
 
 /************************************************************************/
 /*									*/
@@ -21,60 +20,77 @@
 /************************************************************************/
 
 #ifndef BYTESWAP
-	/********************************************************/
-	/*   Normal byte-order definitions, for e.g., 68020s	*/
-	/********************************************************/
+/********************************************************/
+/*   Normal byte-order definitions, for e.g., 68020s	*/
+/********************************************************/
 
 /* These are the TOS manipulation Macros */
 
-#define HARD_PUSH(x)	*(CSTKPTRL++) = x
-#define PUSH(x)		{HARD_PUSH(TOPOFSTACK); TOPOFSTACK = x;}
-#define POP		TOPOFSTACK = *(--CSTKPTRL)
-#define GET_TOS_1	*(CSTKPTR - 1)
-#define GET_TOS_2	*(CSTKPTR - 2)
-#define GET_POPPED	*CSTKPTR
-#define POP_TOS_1	*(--CSTKPTRL)
-#define TOPOFSTACK	tscache
-#define GET_TOS_1_HI	*((DLword *)(CSTKPTR - 1))
-#define GET_TOS_1_LO	*((DLword *)(CSTKPTR - 1)+1)
+#define HARD_PUSH(x) *(CSTKPTRL++) = x
+#define PUSH(x)            \
+  {                        \
+    HARD_PUSH(TOPOFSTACK); \
+    TOPOFSTACK = x;        \
+  }
+#define POP TOPOFSTACK = *(--CSTKPTRL)
+#define GET_TOS_1 *(CSTKPTR - 1)
+#define GET_TOS_2 *(CSTKPTR - 2)
+#define GET_POPPED *CSTKPTR
+#define POP_TOS_1 *(--CSTKPTRL)
+#define TOPOFSTACK tscache
+#define GET_TOS_1_HI *((DLword *)(CSTKPTR - 1))
+#define GET_TOS_1_LO *((DLword *)(CSTKPTR - 1) + 1)
 
 #else
 
-	/********************************************************/
-	/*	Byte-swapped definitions, for e.g., 80386s	*/
-	/********************************************************/
+/********************************************************/
+/*	Byte-swapped definitions, for e.g., 80386s	*/
+/********************************************************/
 
 /* These are the TOS manipulation Macros */
 
-#define HARD_PUSH(x)	*(CSTKPTRL++) = x
-#define PUSH(x)		{HARD_PUSH(TOPOFSTACK); TOPOFSTACK = x;}
-#define POP		TOPOFSTACK = *(--CSTKPTRL)
-#define GET_TOS_1	*(CSTKPTR - 1)
-#define GET_TOS_2	*(CSTKPTR - 2)
-#define GET_POPPED	*CSTKPTR
-#define POP_TOS_1	*(--CSTKPTRL)
-#define TOPOFSTACK	tscache
-#define GET_TOS_1_HI	GETWORD((DLword *)(CSTKPTR - 1))
-#define GET_TOS_1_LO	GETWORD((DLword *)(CSTKPTR - 1)+1)
+#define HARD_PUSH(x) *(CSTKPTRL++) = x
+#define PUSH(x)            \
+  {                        \
+    HARD_PUSH(TOPOFSTACK); \
+    TOPOFSTACK = x;        \
+  }
+#define POP TOPOFSTACK = *(--CSTKPTRL)
+#define GET_TOS_1 *(CSTKPTR - 1)
+#define GET_TOS_2 *(CSTKPTR - 2)
+#define GET_POPPED *CSTKPTR
+#define POP_TOS_1 *(--CSTKPTRL)
+#define TOPOFSTACK tscache
+#define GET_TOS_1_HI GETWORD((DLword *)(CSTKPTR - 1))
+#define GET_TOS_1_LO GETWORD((DLword *)(CSTKPTR - 1) + 1)
 
 #endif /* BYTESWAP */
 
-
 /* OPCODE interface routines */
 
-#define StackPtrSave	{CurrentStackPTR = (DLword *) (CSTKPTR-1);}
-#define StackPtrRestore	{CSTKPTRL = ((LispPTR *) CurrentStackPTR)+1;}
+#define StackPtrSave \
+  { CurrentStackPTR = (DLword *)(CSTKPTR - 1); }
+#define StackPtrRestore \
+  { CSTKPTRL = ((LispPTR *)CurrentStackPTR) + 1; }
 
+#define EXT                  \
+  {                          \
+    PC = pccache - 1;        \
+    TopOfStack = TOPOFSTACK; \
+    StackPtrSave;            \
+  }
 
+#define RET                  \
+  {                          \
+    pccache = PC + 1;        \
+    StackPtrRestore;         \
+    TOPOFSTACK = TopOfStack; \
+  }
 
-#define EXT	{ PC=pccache-1;					\
-		  TopOfStack=TOPOFSTACK;			\
-		  StackPtrSave; }
-
-#define RET	{ pccache=PC+1;					\
-		  StackPtrRestore;				\
-		  TOPOFSTACK = TopOfStack; }
-
-#define NRET	{ RET; nextop0; }
+#define NRET \
+  {          \
+    RET;     \
+    nextop0; \
+  }
 
 #endif /* TOS1DEFS_H */
