d8e063bf02446fc53d23e47e440e1f940f2b546f
Bruce Mitchener <bruce.mitchener@gmail.com>
2021-02-23 00:45:07 +0700

    tidy: Remove last `caddr_t`. (#365)
    This cast isn't needed here and `caddr_t` is obsolete / not POSIX.

--- src/rpc.c
+++ src/rpc.c
@@ -121,7 +121,7 @@ LispPTR rpc(LispPTR *args)
   /* Resolve the host address. */
   if (hp) {
     sin1.sin_family = hp->h_addrtype;
-    memcpy((caddr_t)&sin1.sin_addr, hp->h_addr, hp->h_length);
+    memcpy(&sin1.sin_addr, hp->h_addr, hp->h_length);
   } else
     goto handle_error;
 
