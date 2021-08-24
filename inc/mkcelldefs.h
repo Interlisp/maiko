934a86fbb5bda89591911c495ac436de55bdea26
Nick Briggs <nicholas.h.briggs@gmail.com>
2021-08-24 12:05:17 -0700

    Remove Create_N_Set_Cell() which is unused since native code is not supported.

--- inc/mkcelldefs.h
+++ inc/mkcelldefs.h
@@ -3,5 +3,4 @@
 #include "lispemul.h" /* for LispPTR, DLword */
 LispPTR N_OP_createcell(register LispPTR tos);
 DLword *createcell68k(unsigned int type);
-LispPTR Create_n_Set_Cell(unsigned int type, LispPTR value);
 #endif
