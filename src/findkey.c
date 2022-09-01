3786630f333ec902a2a85695c181d5261fb2c231
Nick Briggs <nicholas.h.briggs@gmail.com>
2022-09-01 14:15:36 -0700

    Replace Addr68k related macros with NativeAligned inline functions

--- src/findkey.c
+++ src/findkey.c
@@ -45,7 +45,7 @@ LispPTR N_OP_findkey(LispPTR tos, int byte) {
 #endif
 
   if (CURRENTFX->alink & 1) { /* slow case */
-    find_end = (DLword *)Addr68k_from_LADDR(STK_OFFSET | (CURRENTFX->blink - 4));
+    find_end = (DLword *)NativeAligned2FromLAddr(STK_OFFSET | (CURRENTFX->blink - 4));
   } else { /*  Fast cae */
     find_end = ((DLword *)CURRENTFX) - 2 - 4;
   }
