bf8c1ae277d827e00ae5bb21447d8a2827b150d5
Nick Briggs <nicholas.h.briggs@gmail.com>
2022-09-01 14:15:36 -0700

    Replace Addr68k related macros with NativeAligned inline functions

--- src/bin.c
+++ src/bin.c
@@ -23,7 +23,7 @@
 */
 /**********************************************************************/
 #include "version.h"
-#include "adr68k.h"    // for Addr68k_from_LADDR
+#include "adr68k.h"    // for NativeAligned2FromLAddr, NativeAligned4FromLAddr
 #include "bindefs.h"   // for N_OP_bin
 #include "emlglob.h"
 #include "lispmap.h"   // for S_POSITIVE
