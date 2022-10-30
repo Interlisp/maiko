6e38d50829efca4270459bb9f74c5230f34345e2
Nick Briggs <nicholas.h.briggs@gmail.com>
2022-10-30 15:49:32 -0700

    Improve implementation of word_swap_page() and remove unused code (#447)
    word_swap_page() should only touch each 32-bit word once to change
      the byte order from ABCD to DCBA rather than twice doing a transform
      of the entire region from ABCD to BADC and then again to DCBA
    
      The compiler provided ntohl() usually gets favorable optimizations
      applied, so use it for the byte reordering.
    
      With word_swap_page() rewritten, byte_swap_word() is superfluous.

--- inc/byteswapdefs.h
+++ inc/byteswapdefs.h
@@ -10,16 +10,6 @@ static inline unsigned int swapx(unsigned int word) {
   return (((word >> 16) & 0xffff) | ((word & 0xffff) << 16));
 }
 
-/****************************************************************/
-/*                                                              */
-/*                 Byte-swap a single 2-byte word               */
-/*                                                              */
-/****************************************************************/
-static inline unsigned short byte_swap_word(unsigned short word) {
-  return ((word >> 8) | (unsigned short)((word & 0xff) << 8));
-}
-
-void byte_swap_page(unsigned short *page, int wordcount);
 void word_swap_page(unsigned short *page, int longwordcount);
 void bit_reverse_region(unsigned short *top, int width, int height, int rasterwidth);
 #ifdef RESWAPPEDCODESTREAM
