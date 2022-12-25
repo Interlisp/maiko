960baf9b96dcf86ddd58e8ade3bb6b99b5e91bbe
Nick Briggs <nicholas.h.briggs@gmail.com>
2022-12-25 12:37:23 -0800

    fix signedness issue in N_OP_bind(unused), matching correction made in inlineC.h BIND in 26e67a51

--- inc/bindsdefs.h
+++ inc/bindsdefs.h
@@ -1,7 +1,7 @@
 #ifndef BINDSDEFS_H
 #define BINDSDEFS_H 1
 #include "lispemul.h" /* for LispPTR */
-LispPTR *N_OP_bind(LispPTR *stack_pointer, LispPTR tos, int byte1, int byte2);
+LispPTR *N_OP_bind(LispPTR *stack_pointer, LispPTR tos, unsigned byte1, unsigned byte2);
 LispPTR *N_OP_unbind(LispPTR *stack_pointer);
 LispPTR *N_OP_dunbind(LispPTR *stack_pointer, LispPTR tos);
 #endif
