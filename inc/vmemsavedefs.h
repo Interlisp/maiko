c4752e6caab2f63ff1aa8fdb21a1cd6cc9b6cdb7
Nick Briggs <nicholas.h.briggs@gmail.com>
2025-07-26 17:54:17 -0700

    Makes vmemsave.c lispstringP implementation local and updates header file accordingly

--- inc/vmemsavedefs.h
+++ inc/vmemsavedefs.h
@@ -1,7 +1,6 @@
 #ifndef VMEMSAVEDEFS_H
 #define VMEMSAVEDEFS_H 1
-#include "lispemul.h" /* for LispPTR, DLword */
-int lispstringP(LispPTR Lisp);
+#include "lispemul.h" /* for LispPTR */
 LispPTR vmem_save(char *sysout_file_name);
 LispPTR vmem_save0(LispPTR *args);
 void lisp_finish(int exit_status);
